7df1377a219873a91eb0a09ca0d06a699fce1ec3
Shubham Dwivedi <shubhamdwivedivns09@gmail.com>
2026-08-23 17:50:19 +0530

    Variable commit

---
+++ 01_Basics/02_Data_types.js
@@ -0,0 +1,11 @@
+"use Strinct"; // treat all js code as newer version 
+// alert(3+3) // we are using note js, not browser
+
+let age=18
+let isLoggined=false
+let state = null
+
+let x; //undefined
+
+console.log(typeof undefined ) //Undefine
+console.log(typeof null) //object
