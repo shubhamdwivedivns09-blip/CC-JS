98b6081cd0f28af87f870f81e1aa2f8e58b1e523
Shubham Dwivedi <shubhamdwivedivns09@gmail.com>
2026-08-23 18:36:08 +0530

    conversionOperators

---
+++ 01_Basics/03_conersionOper.js
@@ -0,0 +1,23 @@
+let score = "23";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score)
+// console.log(typeof (valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; False=>1;
+
+let isLoggedIn = 0
+let booleanIsLogged=Boolean(isLoggedIn)
+
+// console.log(booleanIsLogged)
+// 1 => true; 0 => false
+// "" => false
+// "shubham" => true
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
