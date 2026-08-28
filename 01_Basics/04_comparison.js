b0e52b090318ac240291513d5e48a18654aaf526
Shubham Dwivedi <shubhamdwivedivns09@gmail.com>
2026-08-28 22:12:22 +0530

    01_Basics/03_ConversionOperator.js

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,21 @@
+console.log("2">1);
+console.log("03">1);
+
+console.log();
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log();
+
+console.log(undefined==0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log();
+
+// === 
+
+console.log("3"===3);
+
