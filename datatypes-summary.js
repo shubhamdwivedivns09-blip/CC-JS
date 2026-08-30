b0cccc3ab9aac740cf068719729a181c248a6923
Shubham <shubhamdwivedi@Shubhams-MacBook-Air.local>
2026-08-31 01:07:18 +0530

    Update JavaScript files

---
+++ datatypes-summary.js
@@ -0,0 +1,19 @@
+
+
+// Stack(Primitive) , Heap(Primitive)
+let myYtname="shubhamdwivedi"
+let othername=myYtname
+console.log(othername);
+
+let userone = {
+    email : "user@google.com",
+    upi :"user@ybl"
+}
+
+let userTwo=userone
+
+userTwo.email="shubham@mail.com"
+
+console.log(userone.email);
+console.log(userTwo);
+
--- string.js
+++ string.js
@@ -2,7 +2,35 @@ const name = "shubham"
 const repoCount=30;
 
 
-console.log(name+repoCount+"sam");
+// console.log(name+repoCount+"sam");
 
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
- 
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const game=new String("Shubham")
+
+// console.log(game[0]);
+// console.log(game[1]);
+
+// console.log(game.length);
+// console.log(game.toUpperCase());
+
+ console.log(game.charAt(3));
+console.log(game.indexOf("b"));
+
+const newString = game.substring(0,4)
+console.log(newString);
+const otherString = game.substring(-8,2)
+console.log(otherString);
+
+const newStringOne = "        Shubham          "
+console.log(newStringOne);
+console.log(newStringOne.trim( ));
+
+const url = "https://shubham.com/shubham%20dwivedi"
+
+console.log(url.replace('%20','-'))
+console.log(url.includes('dsshubham'));
+
+console.log(game.split('-'));
+
