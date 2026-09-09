6ec4ae6e7ce4ac84b7d6509f4e81306f55c3ce3a
Shubham <shubhamdwivedi@Shubhams-MacBook-Air.local>
2026-09-09 22:47:36 +0530

    Arrow Function

---
+++ 03_Basics/02_ArrowFun.js
@@ -0,0 +1,46 @@
+const user = {
+    username: "Shubham",
+    price: 999,
+
+    welcomeMassage: function(){
+        console.log(`${this.username} , Welcom to Website`);
+       // console.log(this);
+        
+    }
+}
+// user.welcomeMassage();
+// user.username="ady"
+// user.welcomeMassage()
+
+// console.log(this);
+
+// function xyz(){
+//     let username = "Shubham"
+//     console.log(this.username);
+// }
+// xyz() // Not applicable in function it is only applicable in Object
+
+// const s = function xyz(){
+//     let username = "Shubham"
+//     console.log(this.username);
+// }
+// xyz()
+
+// const s = () => {
+//     let username = "Shubham"
+//     console.log(this  );
+// }
+// s();
+
+const addTwo = (num1, num2) =>{ //if we write in curly braces then we Should use return => EXPLICIT RETURN
+    return num1+num2;
+}
+// console.log(addTwo(45,56))
+
+// const addTwo = (num1,num2) => (num1+num2);// if we wrap in paranthesis then there is no use of return => IMPLICIT RETURN 
+
+// const addtwo = (num1,num2) => ({username : "Shubham"})
+// console.log(addtwo(3,4));
+
+const add = (a, b) => a + b;
+console.log(add(3,4));
